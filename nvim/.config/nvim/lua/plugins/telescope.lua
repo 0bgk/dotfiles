0d35ab5dcd458228a93d4778698c20f7800a8b4b
0bgk <orlandohector41@gmail.com>
2026-03-13 16:42:42 +0100

    feat(nvim): add telescope with fzf-native, but will it be fast?

---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      defaults = {
+        file_ignore_patterns = { "node_modules", "%.git/", "dist", "build" },
+        layout_strategy = "horizontal",
+        layout_config = {
+          prompt_position = "top",
+          preview_width = 0.5,
+          winblend = 0,
+        },
+        sorting_strategy = "ascending",
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+      },
+    },
+    config = function(, opts)
+      require("").setup(opts)
+      require("telescope").load_extension("fzf")
+    end,
+  },
+}
